51ef7f669edd94339371b92efc507909bb71493f
Philip <philipmjobe@gmail.com>
2021-11-11 17:34:19 -0500

    added html and javascript

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 var app = new Vue({
   el: "#app",
   data: function () {
-    message: "Hello from JavaScript!";
-    movies: [
-      { id: 1, title: "Saw" },
-      { id: 2, title: "Halloween" },
-      { id: 3, title: "Hellraiser" },
-      { id: 4, title: "Terrifier" },
-    ];
+    return {
+      message: "Hello from JavaScript!",
+      movies: ["Saw", "Halloween", "Terrifier"],
+      newMovie: "",
+    };
+  },
+  methods: {
+    addMovie: function () {
+      this.movies.push(this.newMovie);
+      this.newMovie = "";
+    },
   },
 });
